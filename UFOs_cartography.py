6d3f066ff1c79e83a392892c68efaf7b1283db9f
JulesBrable <93934635+JulesBrable@users.noreply.github.com>
2022-12-02 11:57:49 +0100

    Jules (#17)
    * First addition of a notebook
    
    * cleaning data and begening the streamlit app
    
    * Cleaning geospatial data for the interactive map
    
    * Adding clean data --data
    
    d
    d
    
    * Jules (#1) (#2)
    
    * First addition of a notebook
    
    * cleaning data and begening the streamlit app
    
    * Cleaning geospatial data for the interactive map
    
    * Update .gitignore
    
    * Adding requirements
    
    * updating requirements
    
    * No changes in the map, just for testing
    
    * big advancements in country column cleaning
    
    * cleaning of city column
    
    * Multiple filtering
    
    * Cleaning shape, duration and city columns
    
    * Fixing issue due to mis-filtering the dataframe
    
    * Customizing Markers+tooltip+information
    
    * Adding geopy, bs4 and requests
    
    * Removing pip install
    
    * New cleaned csv
    
    * Removing duration former 'duration multiselect'
    
    * Enhancing the README
    
    * Specifying version of all packages
    
    * Splitting the code and adding images
    
    * Adding pictures for the app
    
    * Splitting the app code into functions
    
    * Fixing issue due to anterior numpy version
    
    * Adding application interface preview
    
    * Adding a detailed description
    
    * Adding email
    
    * Cleaning the scripts removing some comments
    
    * removing useless dependencies
    
    * Adding plot to the app
    
    * Adding documentation, tabs within the app  and statistical summary

--- UFOs_cartography.py
+++ UFOs_cartography.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 import streamlit as st
 from streamlit_folium import folium_static
 import folium
@@ -11,13 +13,13 @@ if __name__ == '__main__':
     
     st.set_page_config(
         page_title="UFOs Sightings Reports Map",
-        page_icon="🛸",
+        page_icon="🛸", # this icon appears on the tab of the web page
         layout="centered",
         initial_sidebar_state="expanded",
         menu_items={
             'Report a bug': "https://github.com/JulesBrable/UFO/issues/new",
             'About': """ 
-            If you want to read more about the project, you be interested in going to the corresponding
+            If you want to read more about the project, you would be interested in going to the corresponding
             [GitHub](https://github.com/JulesBrable/UFO) repository.
             
             Contributions:
@@ -34,13 +36,14 @@ if __name__ == '__main__':
     #@st.cache(allow_output_mutation=True) # for improved performance in (re-)loading data
     df = get_data("data/ufo_sightings_clean.csv")
     
-    cols_list = ['country', 'city', 'shape']
+    cols_list = ['country', 'city', 'shape', 'state']
     df[cols_list] = df[cols_list].apply(clean_col)
     
     df['year_UFO'] = pd.DatetimeIndex(df['datetime']).year
+    
+    st.sidebar.subheader(f"Please filter the desired features:")
         
     with st.container():
-        
         st.header(
             f"Mapping of UFO sightings reports from {min(df['year_UFO'])} to {max(df['year_UFO'])}"
         )
